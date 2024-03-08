c2565c77ee8d2f638973a4a62ff054023d99aa8d
kai <kai.wei@wavein.com.tw>
2024-03-08 15:12:58 +0800

    update

--- server_express/index.mjs
+++ server_express/index.mjs
@@ -16,10 +16,7 @@ app.get('/json', (req, res) => {
 
 // 定義一個路由處理程序，當收到 GET 請求時查詢 PostgreSQL 資料庫中的 users 表格
 app.get('/users', (req, res) => {
-	// 設置要使用的 Schema
-	// const schemaName = 'prac'; // 替換為你想要使用的 Schema 名稱
-	// client.query(`SET search_path TO ${schemaName}`);
-
+	
 	client.query('SELECT * FROM prac.users', (err, result) => {
 		if (err) {
 			console.error('Query error', err.stack);
