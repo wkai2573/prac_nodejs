75200136d17d4f2fcf3499f22c75b2aacc293536
kai <kai.wei@wavein.com.tw>
2024-03-22 16:21:46 +0800

    feat: 優化user操作

--- server_apollo/gql/resolver/Mutation/user.mjs
+++ server_apollo/gql/resolver/Mutation/user.mjs
@@ -2,23 +2,18 @@ import bcrypt from 'bcryptjs';
 
 export async function userCreate(parent, args, context, info) {
 	//! 驗證登入
-
 	const {input: {account, name, password}} = args;
-	try {
-		// 新增user 寫入db
-		const query = 'INSERT INTO prac.users (account, name, password) VALUES ($1, $2, $3) RETURNING *';
-		const values = [
-			account,
-			name,
-			bcrypt.hashSync(password, 10),
-		];
-		const result = await context.db.pool.query(query, values);
-		const newUser = result.rows[0];
-		return newUser;
-	} catch (error) {
-		console.error('Error inserting data:', error);
-		throw error;
-	}
+	
+	// 新增user 寫入db
+	const query = 'INSERT INTO prac.users (account, name, password) VALUES ($1, $2, $3) RETURNING *';
+	const values = [
+		account,
+		name,
+		bcrypt.hashSync(password, 10),
+	];
+	const result = await context.db.pool.query(query, values);
+	const newUser = result.rows[0];
+	return newUser;
 }
 
 export async function userUpdate(parent, args, context, info) {
@@ -26,34 +21,24 @@ export async function userUpdate(parent, args, context, info) {
 	const {id, input} = args;
 	if (input.password) input.password = bcrypt.hashSync(input.password, 10);
 
-	try {
-		var i = 1;
-		const setClause = Object.keys(input).map(key => `${key} = $${i++}`).join(',');
-		const query = `
-			UPDATE prac.users
-			SET ${setClause}
-			WHERE id = $${i++}
-			RETURNING *`;
-		const values = [...Object.values(input), id];
-		const result = await context.db.pool.query(query, values);
-		const user = result.rows[0];
-		return user;
-	} catch (error) {
-		console.error('Error updating user:', error);
-		throw error;
-	}
+	var i = 1;
+	const setClause = Object.keys(input).map(key => `${key} = $${i++}`).join(',');
+	const query = `
+		UPDATE prac.users
+		SET ${setClause}
+		WHERE id = $${i++}
+		RETURNING *`;
+	const values = [...Object.values(input), id];
+	const result = await context.db.pool.query(query, values);
+	const user = result.rows[0];
+	return user;
 }
 
 export async function userDelete(parent, args, context, info) {
 	//! 驗證登入
 	const {id} = args;
-	try {
-		const query = 'DELETE FROM prac.users WHERE id = $1';
-		const values = [id];
-		const result = await context.db.pool.query(query, values);
-		return 'ok';
-	} catch (error) {
-		console.error('Error deleting user:', error);
-		throw error;
-	}
+	const query = 'DELETE FROM prac.users WHERE id = $1';
+	const values = [id];
+	const result = await context.db.pool.query(query, values);
+	return 'ok';
 }
--- server_apollo/gql/resolver/Query/user.mjs
+++ server_apollo/gql/resolver/Query/user.mjs
@@ -1,13 +1,7 @@
 export async function users(parent, args, context, info) {
 	//! 驗證登入
-	
-	try {
-		const query = 'SELECT * FROM prac.users';
-		const result = await context.db.pool.query(query);
-		const users = result.rows;
-		return users;
-	} catch (error) {
-		console.error('Error inserting data:', error);
-		throw error;
-	}
+	const query = 'SELECT * FROM prac.users';
+	const result = await context.db.pool.query(query);
+	const users = result.rows;
+	return users;
 }
